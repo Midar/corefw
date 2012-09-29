0452bfc9ef58a723ff846de988dc2a898c7de5dd
Jonathan Schleifer <js@webkeks.org>
2012-09-30 00:12:36 +0200

    Update corefw.h.

--- src/corefw.h
+++ src/corefw.h
@@ -3,7 +3,17 @@
 
 #include "class.h"
 #include "object.h"
-#include "string.h"
 #include "array.h"
+#include "bool.h"
+#include "box.h"
+#include "double.h"
+#include "file.h"
+#include "hash.h"
+#include "int.h"
+#include "map.h"
+#include "range.h"
+#include "refpool.h"
+#include "stream.h"
+#include "string.h"
 
 #endif
